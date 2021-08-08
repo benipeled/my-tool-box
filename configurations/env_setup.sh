e02922a198b639ab3cabf572f581d2150fdae062
Beni Peled <benipeled@gmail.com>
2021-08-08 21:48:42 +0300

    Add env_setup.sh script for setting up a new workstation (env.)

---
+++ configurations/env_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Run this script directly by:
+# 	curl -s https://raw.githubusercontent.com/benipeled/my-tool-box/main/configurations/env_setup.sh | bash
+
+PACKAGES="wget vim ansible git flameshot \
+	yakuake keepass gnome-tweaks \
+	podman podman-compose awscli ipython telnet vim-default-editor
+	"
+
+
+# Install my packages
+sudo dnf install -y $PACKAGES
+
+# Update .bashrc file (if not updated)
+if ! grep -q "My Bashrc Modificatoins" ~/.bashrc
+then
+	curl https://raw.githubusercontent.com/benipeled/my-tool-box/main/configurations/bashrc >> ~/.bashrc
+fi
+
+# Create git repositories folder
+mkdir ~/repos
+
+# Install snapd
+sudo dnf install -y snapd
+sudo ln -s /var/lib/snapd/snap /snap
+
+
+# Install pycharm
+sudo snap install pycharm-community --classic
