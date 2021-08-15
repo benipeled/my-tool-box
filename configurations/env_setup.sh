3c753f9fd96b97803db63bf1e3725355e18a165c
Beni Peled <benipeled@gmail.com>
2021-08-15 16:44:47 +0300

    Add clone stages

--- configurations/env_setup.sh
+++ configurations/env_setup.sh
@@ -2,17 +2,20 @@
 # Run this script directly by:
 # 	curl -s https://raw.githubusercontent.com/benipeled/my-tool-box/main/configurations/env_setup.sh | bash
 
-# Add repos
-sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-
-
 PACKAGES="wget vim ansible git flameshot \
 	yakuake keepass gnome-tweaks \
 	podman podman-compose awscli ipython telnet vim-default-editor gh
 	"
 
+REPO_FOLDER=~/repos
 
-# Install my packages
+
+
+# Add repos
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+
+# Update; Install my packages
+sudo dnf upgrade -y
 sudo dnf install -y $PACKAGES
 
 # Update .bashrc file (if not updated)
@@ -21,13 +24,19 @@ then
 	curl https://raw.githubusercontent.com/benipeled/my-tool-box/main/configurations/bashrc >> ~/.bashrc
 fi
 
-# Create git repositories folder
-mkdir ~/repos
-
 # Install snapd
 sudo dnf install -y snapd
 sudo ln -s /var/lib/snapd/snap /snap
 
-
 # Install pycharm
 sudo snap install pycharm-community --classic
+
+# Create git repositories folder
+mkdir $REPO_FOLDER
+
+# Clone git repos
+cd $REPO_FOLDER
+git clone git@github.com:benipeled/my-tool-box.git
+git clone git@github.com:benipeled/scylla-pkg.git
+git clone git@github.com:benipeled/scylla-machine-image.git
+git clone git@github.com:awslabs/aws-cloudformation-templates.git
