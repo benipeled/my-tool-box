b2ac2aaa7406c6d734025e732753aebed39c88b5
Beni Peled <benipeled@gmail.com>
2021-08-19 12:58:52 +0300

    env_setup: Update packages

--- configurations/env_setup.sh
+++ configurations/env_setup.sh
@@ -2,19 +2,19 @@
 # Run this script directly by:
 # 	curl -s https://raw.githubusercontent.com/benipeled/my-tool-box/main/configurations/env_setup.sh | bash
 
-PACKAGES="wget vim ansible git flameshot \
+PACKAGES="wget vim ansible git flameshot nmap \
 	yakuake keepass gnome-tweaks \
 	podman podman-compose awscli ipython telnet vim-default-editor gh
 	"
-
+REMOVE_PACKAGES="nano-default-editor"
 REPO_FOLDER=~/repos
-
 
 
 # Add repos
 sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
 
-# Update; Install my packages
+# Remove unwanted packages; Update; Install my packages
+sudo dnf remove -y $REMOVE_PACKAGES
 sudo dnf upgrade -y
 sudo dnf install -y $PACKAGES
 
