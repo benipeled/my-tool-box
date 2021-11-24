34e2a421afa807e1e53476b3d0bbe6b4ec121278
Beni <benipeled@gmail.com>
2021-11-24 17:59:10 +0200

    Add a few run_command implementions

--- configurations/env_setup.sh
+++ configurations/env_setup.sh
@@ -51,9 +51,9 @@ function run_command {
 ########### Configurations #############
 
 # Add repos
-sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo # add github repo
-sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo # add hashicorp repo
-sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm # add rpmfusion repo
+run_command "Add github RPM repository" sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+run_command "Add hashicorp RPM repository" sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+run_command "Add rpmfusion RPM repository" sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 
 # Add Google Chrome repo
 echo '[google-chrome]
@@ -64,19 +64,18 @@ gpgcheck=1
 gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub' | sudo tee /etc/yum.repos.d/google-chrome.repo >/dev/null
 
 
-# Update .bashrc file (if not updated)
 if ! grep -q "My bash modificatoins" ~/.bashrc
 then
-	curl https://raw.githubusercontent.com/benipeled/my-tool-box/main/configurations/bashrc >> ~/.bashrc
+	run_command "Update .bashrc file" curl https://raw.githubusercontent.com/benipeled/my-tool-box/main/configurations/bashrc >> ~/.bashrc
 fi
 
 
 ########### Installations #############
 
 # Remove unwanted packages; Update; Install my packages
-sudo dnf remove -y $REMOVE_PACKAGES
-sudo dnf upgrade -y
-sudo dnf install -y $PACKAGES
+run_command "Remove unwanted packages" sudo dnf remove -y $REMOVE_PACKAGES
+run_command "Update packages" sudo dnf upgrade -y
+run_command "Install packages" sudo dnf install -y $PACKAGES
 
 # Install snapd
 sudo dnf install -y snapd
@@ -87,12 +86,12 @@ sudo snap install pycharm-community --classic
 
 # Install pip packages
 for package in $PIP_PACKAGES; do
-  pip show -q $package && echo "pip: $package is already installed" || pip install $package
+  pip show -q $package && echo "pip: $package is already installed" || run_command "Install $package" pip install $package
 done
 
 # Install npm packages
 for package in $NPM_PACKAGES; do
-  sudo npm list --global npm-groovy-lint 1> /dev/null && echo "npm: npm-groovy-lint is already installed" || sudo npm install --global npm-groovy-lint
+  sudo npm list --global npm-groovy-lint 1> /dev/null && echo "npm: npm-groovy-lint is already installed" || run_command "Install $package" sudo npm install --global npm-groovy-lint
 done
 
 ########### GIT #############
@@ -107,13 +106,11 @@ for repo in $MY_GIT_REPOS; do
 done
 popd
 
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install -y flathub org.gnome.Extensions
+run_command "Add flatpak repo (flathub)" flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+run_command "Install gnome extension (Extensions)" flatpak install -y flathub org.gnome.Extensions
 
 # Configure global git user
-
-git config --global user.name "Beni Peled"
-git config --global user.email benipeled@gmail.com
+run_command "Configure global git user" git config --global user.name "Beni Peled" && git config --global user.email benipeled@gmail.com
 
 # Gnome Extensions
 gnome-extensions disable background-logo@fedorahosted.org
