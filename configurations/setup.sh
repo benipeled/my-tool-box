d14db1f882a038d21073e8af3b602ff2a77943dc
Beni Peled <benipeled@gmail.com>
2024-11-03 10:15:17 +0200

    Add docker installation

--- configurations/setup.sh
+++ configurations/setup.sh
@@ -141,9 +141,16 @@ install_forticlient() {
   run_command "Installing FortiClient $FORTICLIENT_VERSION" "sudo apt-get update && sudo apt-get install -y ./forticlient_vpn_$FORTICLIENT_VERSION_amd64.deb"
 }
 
+install_docker() {
+  run_command "Installing Docker dependencies" "sudo apt-get update && sudo apt-get install -y ca-certificates curl"
+  run_command "Downloading Docker GPG key" "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && sudo chmod a+r /etc/apt/keyrings/docker.asc"
+  run_command "Adding Docker apt repository" "echo \"deb [arch=\$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \$(. /etc/os-release && echo \\\"\$VERSION_CODENAME\\\") stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null"
+  run_command "Installing Docker packages" "sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose"
+  run_command "Granting docker privileges" "sudo usermod -aG docker $USER"
+}
+
 # Add repos
 run_command "Add Google Chrome repo" "wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add - && echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list"
-#run_command "Add Docker repo" "sudo apt-get update && sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable'"
 
 # Update .bashrc file if it doesn't already include custom modifications
 if ! grep -q "My bash modifications" ~/.bashrc; then
@@ -251,3 +258,4 @@ EOF"
 
 install_pycharm
 install_forticlient
+install_docker
